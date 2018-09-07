66746272c1801a4e18d00d19ca80748eee51365e
grandballoon <luke.sovern.conley@gmail.com>
2018-09-07 13:02:41 -0400

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-
+import { Menu } from 'semantic-ui-react'
 
 const Header = (props) => {
-
+  const textAlignment = {color: 'white', width:'auto', padding: 10, margin: 10}
   return (
   <div>
-    YOLO
+    <div class="ui blue inverted menu">
+      <h2 style={textAlignment}>Goooooaaaaal</h2>
+    </div>
   </div>
 )
 
